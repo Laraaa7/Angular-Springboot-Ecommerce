ecdd4459351dbe2cdca8e5fe22944afcb162b41e
lara <larasonodelaviuda@gmail.com>
2025-11-12 18:43:02 +0100

    Eliminación de archivos

--- 03-frontend/angular-ecommerce/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    shopApiUrl: "https://localhost:8443/api",
-    stripePublishableKey: "pk_test_51SSbKc38REJyHx0eDKi2w32nN8DHpZHgytQWUG9hggupJRmbL0usCxFzQejJygbNGeIRbAnY21hDFic9R21y8Vga00o4Xg7r8l"
-};
--- 03-frontend/angular-ecommerce/src/environments/environment.qa.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    shopApiUrl: "https://localhost:9898/api"
-};
